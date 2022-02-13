0fa8b7ba9a86b23bc8ebda0167d41353a6f1011e
Zachary Gichiri <gichirigichuru@gmail.com>
2022-02-13 21:30:32 +0300

    add quote class

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete: boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
